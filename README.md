13de9fbb62f0234960df19567740f67dd1e94151
Xavier Jorda <jorda.murria.xavier@gmail.com>
2021-05-14 08:00:14 +0200

    adding docker for the postgres sql db

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # todo-python
+
+## Docker
+#### Start PostgresSQL
+from the ./docker directory
+docker-compose up
---
+++ docker/db/01-init.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE udacity;
+GRANT ALL PRIVILEGES ON DATABASE udacity TO postgres;
+
+
+-- switch to melco db
+\c udacity
