5b9d159aa3ec39ac2f4bee5282f214ce593945c8
Xavier Jorda <jorda.murria.xavier@gmail.com>
2021-05-17 07:01:27 +0200

    adding create route

--- app.py
+++ app.py
@@ -1,25 +1,52 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import query
+import sys
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres:tgkicksass@localhost:15432/udacity'
 
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+  __tablename__='todos'
+  id = db.Column(db.Integer, primary_key=True)
+  description = db.Column(db.String, nullable=False)
+  
+  def __repr__(self) -> str:
+      return f'<Todo {self.id} {self.description}>'
+
+
+db.create_all()
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    todo = Todo(description=description)
+    db.session.add(todo)
+    db.session.commit()
+    body['description'] = todo.description
+
+  except:
+    error = True
+    db.session.rollback
+    print(sys.exc_info())
+  finally:
+    db.session.close()
+
+  if error:
+    abort (400)
+  else:
+    return jsonify(body)
 
 @app.route('/')
 def index():
-    return render_template('index.html', data=[
-      {
-        'description': 'Todo 1'
-      },
-            {
-        'description': 'Todo 2'
-      },
-            {
-        'description': 'Todo 3'
-      }
-    ])
+    return render_template('index.html', data=Todo.query.all())
 
 
 if __name__ == '__main__':
